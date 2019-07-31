3f3cf377d0fd482b17a3c34e06160c7743f60be7
SeulgiSeong <seulgi.seong@nhnent.com>
2019-07-31 14:40:54 +0900

    프로덕션 빌드 포트 수정

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,30 +1,32 @@
 module.exports = {
-    apps: [{
-        name: 'DoorayGiphy',
-        script: './dist/index.js',
-        "ignore_watch": ['node_modules'],
-        "watch_options": {
-            "followSymlinks": false
-        },
-        env: {
-            NODE_ENV: 'development',
-            watch: true,
-            PORT: 3000
-        },
-        env_production: {
-            NODE_ENV: 'production',
-            PORT: 8080
-        }
-    }],
-
-    deploy: {
-        production: {
-            user: 'ne11243',
-            host: 'doocon.nhnent.com',
-            ref: 'origin/develop',
-            repo: 'https://github.com/seulgi0711/dooray_giphy.git',
-            path: '/home1/ne11243/test',
-            'post-deploy': 'npm install && npm run build'
-        }
+  apps: [
+    {
+      name: "DoorayGiphy",
+      script: "./dist/index.js",
+      ignore_watch: ["node_modules"],
+      watch_options: {
+        followSymlinks: false
+      },
+      env: {
+        NODE_ENV: "development",
+        watch: true,
+        PORT: 3000
+      },
+      env_production: {
+        NODE_ENV: "production",
+        PORT: 10145
+      }
     }
+  ],
+
+  deploy: {
+    production: {
+      user: "ne11243",
+      host: "doocon.nhnent.com",
+      ref: "origin/develop",
+      repo: "https://github.com/seulgi0711/dooray_giphy.git",
+      path: "/home1/ne11243/test",
+      "post-deploy": "npm install && npm run build"
+    }
+  }
 };
